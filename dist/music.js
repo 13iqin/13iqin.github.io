994cc84a16d1ce44a63d8d2f6370fa1d248d7703
biqin <miranda@seeapp.com>
2019-05-14 14:51:26 +0800

    Site updated: 2019-05-14 14:51:25

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "canon in d",
+        artist: 'Brian Crain',
+        url: 'http://www.ytmp3.cn/down/43418.mp3',
+        cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
+      }
+    ]
+});
